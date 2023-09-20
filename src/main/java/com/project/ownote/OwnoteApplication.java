398d8243d29714cc74500927ac9a700d22361014
Jlostcode <Jlostcode@github.com>
2023-09-20 17:37:35 +0900

    23.09.20 -1차 프로젝트 배포(ownote)

---
+++ src/main/java/com/project/ownote/OwnoteApplication.java
@@ -0,0 +1,13 @@
+package com.project.ownote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ownote/OwnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ownote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
